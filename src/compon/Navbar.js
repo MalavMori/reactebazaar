9b13c02c5f29ccd52b7e334805fadb1146b24d34
MalavMori <92566896+MalavMori@users.noreply.github.com>
2021-10-26 13:48:59 +0530

    Update Navbar.js

--- src/compon/Navbar.js
+++ src/compon/Navbar.js
@@ -3,7 +3,7 @@ import productdata from './productsdata';
 import './scss/navbar.scss';
 import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbara = () => {
     return (
         <>
             <header>
